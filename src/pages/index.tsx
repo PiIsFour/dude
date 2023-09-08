148b26c43700f98d4c2f9eec6472bc484e6dd77a
PiIsFour <PiIsFour@users.noreply.github.com>
2023-09-08 13:34:06 +0200

    add Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/layout'
 
@@ -7,6 +8,10 @@ export const Head = () => <title>Home Page</title>
 const IndexPage = () => {
 	return <Layout pageTitle="Home Page">
 		<p>I'm making this by following the Gatsby Tutorial.</p>
+		<StaticImage
+			alt="random image of a japanese city at night"
+			src="https://source.unsplash.com/random/500/?japan,city,night"
+		/>
 	</Layout>
 }
 
